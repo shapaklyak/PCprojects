26e987c43a8681f5302317420243431686c407b8
Katya <shapaklyak@users.noreply.github.com>
2018-07-09 17:01:17 -0400

    Create MSDsubsetPC.R
    subset MSD OUxIM dataset for OGHH dashboard

---
+++ MSDsubsetPC.R
@@ -0,0 +1,30 @@
+*********************************************
+Date updated: 7/09/2018
+Analyst: Katya Noykhovich
+Purpose: To subset OU X IM MSD for OGHH dashboard
+Software: R
+**********************************************
+  
+memory.limit(size = 90000)
+
+getwd()
+library(tidyverse)
+library(dplyr)
+
+#import dataset, edit file name/location as needed
+data <- readr::read_tsv("MER_Structured_Dataset_OU_IM_FY17-18_20180622_v2_1.txt", col_names = TRUE)
+
+#subset for indicators of interest
+sub1 <- subset(data, indicator == 'OVC_SERV' | indicator == 'PP_PREV')
+
+#subset for PC countries:
+sub2 <- subset(sub1, sub1$FundingAgency == "PC")
+
+names(sub2)
+
+#drop any unnecessary columns 
+sub3 <- sub2[c(-(1:2),-(4), -(6), -(22), -(25:26), -(28), -(30), -(34))]
+names(sub3)
+
+#export dataset to csv/txt for dashboard
+write.csv(sub3,"OGHHdash_MSD_OUxIM_20180622.csv", row.names = FALSE)
